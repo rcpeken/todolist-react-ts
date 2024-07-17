ca723ffdb41c92bd1ea3cb06ea78be39b1a55d52
rcpeken <recepeken113@gmail.com>
2024-07-17 19:51:31 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ChangeEvent, FC} from 'react';
 import './App.css';
+import { useState } from 'react';
+import { text } from 'stream/consumers';
+import { todoType } from './apptypes';
+import TodoItem from './TodoItem';
 
-function App() {
+
+
+
+const App: FC=()=> {
+  const [task, setTask] = useState<string>('')
+  const [workDay, setWorkDay] = useState<number>(0)
+  const [todoList, setTodoList] = useState<todoType[]>([])
+
+const handleChange =(event:ChangeEvent<HTMLInputElement>)=>{
+if (event.target.name==='task') {
+  setTask(event.target.value)
+}else{
+  setWorkDay(Number(event.target.value))
+}
+};
+const addNewTask =()=>{
+  const newTask ={taskName:task,workDay:workDay}
+  setTodoList([...todoList,newTask])
+  setTask('');
+  setWorkDay(0);
+}
+const deleteTask=(nameToDelete:string) : void=>{
+setTodoList(
+  todoList.filter((task)=>{
+    return task.taskName !== nameToDelete 
+  })
+)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='mainCard'>
+        <input className='maincardinput' 
+        type="text"
+         value={task} 
+         name='task'
+         placeholder='Enter your task..'
+          onChange={handleChange} 
+          />
+        <input className='maincardinput'
+         type="number"
+          value={workDay}
+          name='workDay'
+           placeholder='How many days you have to complete it' 
+           onChange={handleChange} 
+           />
+        <button className='mainCardButton' onClick={addNewTask}>Add new task</button>
+      </div>
+      <div className='todocard'>
+        {todoList.map((task:todoType,index:number)=>{
+          return <TodoItem key={index} task={task} deleteTask={deleteTask}/>
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/TodoItem.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { todoType } from './apptypes'
+
+
+type PropsType={
+    task:todoType
+    deleteTask(nameToDelete:string):void
+}
+
+function TodoItem({task,deleteTask}:PropsType) {
+  return (
+    <div className='card'>
+        <div>
+            <p>{task.taskName}</p>
+            <p>{task.workDay}</p>
+            <button onClick={()=>deleteTask(task.taskName)}>Delete</button>
+        </div>
+    </div>
+  )
+}
+
+export default TodoItem
